46f48aa4baeb8463776f4ee364cf3e54513d6de5
LuduLee <99390489+LuduLee@users.noreply.github.com>
2023-09-27 15:08:16 +0300

    123

---
+++ salon/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BeautySalon
+{
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToServices_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new ServicesPage());
+        }
+    }
+}
---
+++ salon/ServicePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BeautySalon
+{
+    public partial class ServicesPage : Page
+    {
+        public ServicesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ServicesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<int> selectedServicesCosts = servicesListBox.SelectedItems
+                .Cast<ListBoxItem>()
+                .Select(item => int.Parse(item.Tag.ToString()))
+                .ToList();
+
+            int totalCost = selectedServicesCosts.Sum();
+            totalCostTextBlock.Text = $"{totalCost} руб.";
+        }
+    }
+}
